36317bc52cd58a3ddabf57bf30e81330c32bea12
maxgertzen <maxgertzen@gmail.com>
2024-11-11 02:51:30 +1100

    feat(dev): added lint, prettier and installed packages

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,13 +1,20 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettierConfig,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -16,6 +23,8 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier,
+      import: importPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +32,24 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'prettier/prettier': 'error',
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'builtin', // Node.js built-ins
+            'external', // External libraries
+            'internal', // Internal imports
+            'parent', // Parent imports (../)
+            'sibling', // Sibling imports (./)
+            'index', // Index imports
+            'object', // Imports of side effects (e.g., styles)
+            'type', // Type-only imports in TypeScript
+          ],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
     },
-  },
-)
+  }
+);
