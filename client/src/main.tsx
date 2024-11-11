d4ba76fa1c9ea407ec5218d0ad7679330f3c57b0
maxgertzen <maxgertzen@gmail.com>
2024-11-11 21:37:44 +1100

    feat(client): updated index.css & using provider in main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,14 @@ import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import App from './App.tsx';
+import GlobalStyles from './styles/globalStyles.tsx';
+import { ThemeProvider } from './styles/ThemeProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
