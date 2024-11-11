b474ebace0e3b2f1e87eeaf82d1ed3a1e919ad7a
maxgertzen <maxgertzen@gmail.com>
2024-11-11 16:07:41 +1100

    feat(client): arranged the Dashboard page, app entry file with query provider

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,8 +1,16 @@
 import { FC, Suspense } from 'react';
 
 import { ListSidebar, TaskList } from '../../components';
+import { Header } from '../../components/Header/Header';
 import { useLists } from '../../hooks/useLists';
-import { useTaskStore } from '../../store/store';
+import { useTaskStore, useUserStore } from '../../store/store';
+
+import {
+  DashboardContainer,
+  MainLayout,
+  SidebarContainer,
+  TaskContainer,
+} from './Dashboard.styled';
 
 // TODO:
 // - Implement a way to select a list
@@ -14,16 +22,25 @@ export const Dashboard: FC = () => {
   const selectedListId = useTaskStore((state) => state.selectedListId);
   // const setSelectedListId = useTaskStore((state) => state.setSelectedListId);
 
+  const userDetails = useUserStore((state) => state.user);
+
   const { lists } = useLists();
 
   return (
-    <div>
-      <Suspense fallback={<div>Loading lists...</div>}>
-        <ListSidebar lists={lists} /* onSelectList={setSelectedListId} */ />
-      </Suspense>
-      <Suspense fallback={<div>Loading tasks...</div>}>
-        <TaskList selectedListId={selectedListId} />
-      </Suspense>
-    </div>
+    <DashboardContainer>
+      <Header user={userDetails} />
+      <MainLayout>
+        <SidebarContainer>
+          <Suspense fallback={<div>Loading lists...</div>}>
+            <ListSidebar lists={lists} /* onSelectList={setSelectedListId} */ />
+          </Suspense>
+        </SidebarContainer>
+        <TaskContainer>
+          <Suspense fallback={<div>Loading tasks...</div>}>
+            <TaskList selectedListId={selectedListId} />
+          </Suspense>
+        </TaskContainer>
+      </MainLayout>
+    </DashboardContainer>
   );
 };
