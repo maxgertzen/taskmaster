9337c230f78fd77efad258d1ae0d568ee03ec6ef
maxgertzen <maxgertzen@gmail.com>
2024-11-13 19:46:11 +1100

    fix(backend): assigned local client url for dev in .env

--- backend/src/types/global.d.ts
+++ backend/src/types/global.d.ts
@@ -4,6 +4,7 @@ declare namespace NodeJS {
     REDIS_HOST: string;
     REDIS_PORT: string;
     REDIS_PASSWORD: string;
+    LOCAL_DEV_CLIENT_URL: string;
     NODE_ENV: "development" | "production" | "test";
   }
 }
